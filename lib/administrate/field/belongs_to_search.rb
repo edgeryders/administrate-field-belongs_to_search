ae988f129d5d2f9b0b1f7055e5ea5f52cf7c147c
Daniel <daniel@ansorgs.de>
2024-06-13 11:34:44 +0200

    Rename gem

--- lib/administrate/field/belongs_to_search.rb
+++ lib/administrate/field/belongs_to_search.rb
@@ -5,7 +5,7 @@ module Administrate
   module Field
     class BelongsToSearch < Administrate::Field::BelongsTo
       class Engine < ::Rails::Engine
-        initializer 'administrate-field-belongs_to_search.add_assets' do |app|
+        initializer 'annotator-administrate-field-belongs_to_search.add_assets' do |app|
           app.config.assets.precompile << 'belongs_to_search.js' if app.config.respond_to? :assets
           Administrate::Engine.add_javascript 'belongs_to_search.js' if defined?(Administrate::Engine)
         end
